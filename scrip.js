e69ee2bfcf84978c6eab840183aba7bfc3ece106
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2023-04-18 12:19:00 -0300

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -1,4 +1,5 @@
 let display = document.getElementById('input')
+const barra = document.getElementById(uno)
 //boton numero1 
 let btnuno = document.getElementById('uno')
 btnuno.addEventListener('click', () => {
@@ -17,35 +18,55 @@ btntres.addEventListener('click', () => {
 //boton numero4
 let btncuatro = document.getElementById("cuatro")
 btncuatro.addEventListener('click', () => {
-    display.append(btntres.innerHTML)
+    display.append(btncuatro.innerHTML)
 })
 //boton numero5
 let btncinco = document.getElementById("cinco")
 btncinco.addEventListener('click', () => {
-    display.append(btntres.innerHTML)
+    display.append(btncinco.innerHTML)
 })
 //boton numero6
 let btnseis = document.getElementById("seis")
 btnseis.addEventListener('click', () => {
-    display.append(btntres.innerHTML)
+    display.append(btnseis.innerHTML)
 })
 //boton numero7
 let btnsiete = document.getElementById("siete")
 btnsiete.addEventListener('click', () => {
-    display.append(btntres.innerHTML)
+    display.append(btnsiete.innerHTML)
 })
 //boton numero8
 let btnocho = document.getElementById("ocho")
 btnocho.addEventListener('click', () => {
-    display.append(btntres.innerHTML)
+    display.append(btnocho.innerHTML)
 })
 //boton numero9
 let btnnueve = document.getElementById("nueve")
 btnnueve.addEventListener('click', () => {
-    display.append(btntres.innerHTML)
+    display.append(btnnueve.innerHTML)
 })
 //boton numero0
 let btncero = document.getElementById("cero")
 btncero.addEventListener('click', () => {
-    display.append(btntres.innerHTML)
+    display.append(btncero.innerHTML)
+})
+//resta
+let btnmenos = document.getElementById("menos")
+btncero.addEventListener('click', () => {
+    display.append(btnmenos.innerHTML)
+})
+//suma
+let btnmas = document.getElementById("mas")
+btnmas.addEventListener('click', () => {
+    display.append(btnmas.innerHTML)
+})
+//multiplicado
+let btnmultiplicador = document.getElementById("multiplicador")
+btnmultiplicador.addEventListener('click', () => {
+    display.append(btnmultiplicador.innerHTML)
+})
+//dividor
+let btndivisor = document.getElementById("divisor")
+btndivisor.addEventListener('click', () => {
+    display.append(btndivisor.innerHTML)
 })
