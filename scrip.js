66cd46bf1e44b4bbd8c8e543b0aee1b3934fc9e0
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2023-04-18 11:32:02 -0300

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -1,11 +1,51 @@
-
-
-
-
-
-const btn =('uno ')
-btn.addEvenListener("click"),()=>({
-
-
+let display = document.getElementById('input')
+//boton numero1 
+let btnuno = document.getElementById('uno')
+btnuno.addEventListener('click', () => {
+    display.append(btnuno.innerHTML)
 })
-
+//boton numero2
+let btndos = document.getElementById("dos")
+btndos.addEventListener('click', () => {
+    display.append(btndos.innerHTML)
+})
+//boton numero3
+let btntres = document.getElementById("tres")
+btntres.addEventListener('click', () => {
+    display.append(btntres.innerHTML)
+})
+//boton numero4
+let btncuatro = document.getElementById("cuatro")
+btncuatro.addEventListener('click', () => {
+    display.append(btntres.innerHTML)
+})
+//boton numero5
+let btncinco = document.getElementById("cinco")
+btncinco.addEventListener('click', () => {
+    display.append(btntres.innerHTML)
+})
+//boton numero6
+let btnseis = document.getElementById("seis")
+btnseis.addEventListener('click', () => {
+    display.append(btntres.innerHTML)
+})
+//boton numero7
+let btnsiete = document.getElementById("siete")
+btnsiete.addEventListener('click', () => {
+    display.append(btntres.innerHTML)
+})
+//boton numero8
+let btnocho = document.getElementById("ocho")
+btnocho.addEventListener('click', () => {
+    display.append(btntres.innerHTML)
+})
+//boton numero9
+let btnnueve = document.getElementById("nueve")
+btnnueve.addEventListener('click', () => {
+    display.append(btntres.innerHTML)
+})
+//boton numero0
+let btncero = document.getElementById("cero")
+btncero.addEventListener('click', () => {
+    display.append(btntres.innerHTML)
+})
